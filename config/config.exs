f7b56325199637f7876513cd8c94a869198c3a66
Jamu Kakar <jkakar@kakar.ca>
2016-03-15 14:10:10 -0700

    Rename Scientist to Alchemist.

--- config/config.exs
+++ config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure for your application as:
 #
-#     config :scientist, key: :value
+#     config :alchemist, key: :value
 #
 # And access this configuration in your application as:
 #
-#     Application.get_env(:scientist, :key)
+#     Application.get_env(:alchemist, :key)
 #
 # Or configure a 3rd-party app:
 #
